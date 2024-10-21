3a44f4c1849e0bc059a035716a29a948534f0283
bari135 <baribez@post.bgu.ac.il>
2024-10-21 15:58:22 +0300

    added the api/date handeling git status

--- index.js
+++ index.js
@@ -24,6 +24,21 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date_string?', (req, res) => {
+  let date_string = req.params.date_string;
+  let date;
+  if (!date_string) {
+    date = new Date();
+  } else {
+    date = new Date(date_string);
+  }
+  if (date.toString() === 'Invalid Date') {
+    res.json({ error: 'Invalid Date' });
+  } else {
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
+});
+
 
 
 // Listen on port set in environment variable or default to 3000
